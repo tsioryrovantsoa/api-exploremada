bd6926a18b8a26bfe6f260571144cd6472514d93
Tsiory Rakotoarimalala <80772886+tsiory-rovantsoa@users.noreply.github.com>
2023-07-26 20:04:28 +0300

    refactor: bdd script

--- database/script/20230724.sql
+++ database/script/20230724.sql
@@ -5,43 +5,43 @@ create table langue(
 
 INSERT INTO langue VALUES (NULL, 'fr'),(NULL,'en');
 
-create table Utilisateur(
+create table utilisateur(
 	id int auto_increment   primary key,
 	nom varchar(50),
     email varchar(50),
     motdepasse varchar(250),
-    id_langue int  references Langue(id),
+    id_langue int  references langue(id),
     contact varchar(50)
 );
 
 
-create table TypeLieu(
+create table typeLieu(
 	id int auto_increment  primary key,
 	nom varchar(50)
 );
 
-create table Ville(
+create table ville(
 	id int auto_increment  primary key,
 	nom varchar(50),
     remarque text
 );
 
-create table Lieu_Avis(
+create table lieu_Avis(
 	id int auto_increment  primary key,
 	avis_message text,
     photo  varchar(50),
-    id_utilisateur int  references Utilisateur(id)
+    id_utilisateur int  references utilisateur(id)
 );
 
-create table Notification(
+create table notification(
 	id int auto_increment  primary key,
-    id_utilisateur int  references Utilisateur(id),
+    id_utilisateur int  references utilisateur(id),
     contenu text ,
     dateetheure datetime 
 );
 
 
-create table Lieu(
+create table lieu(
 	id int auto_increment  primary key,
 	nom varchar(50),
     description_longue text,
@@ -54,34 +54,34 @@ create table Lieu(
     remarque_frais_entree text,
     contact varchar(50),
     autres_informations text ,
-    id_type_lieu int  references TypeLieu(id),
-    id_ville int  references Ville(id),
+    id_type_lieu int  references typeLieu(id),
+    id_ville int  references ville(id),
     is_populaire varchar(10),
-    id_utilisateur int  references Utilisateur(id)
+    id_utilisateur int  references utilisateur(id)
 );
 
 
-create table Favoris(
+create table favoris(
 	id int auto_increment  primary key,
-    id_utilisateur int  references Utilisateur(id),
-    id_lieu int  references Lieu(id)
+    id_utilisateur int  references utilisateur(id),
+    id_lieu int  references lieu(id)
 );
 
 
-create table Lieu_Image(
+create table lieu_Image(
 	id int auto_increment  primary key,
-    id_lieu int  references Lieu(id),
+    id_lieu int  references lieu(id),
     image varchar(50)
 );
 
-create table Lieu_Video(
+create table lieu_Video(
 	id int auto_increment  primary key,
-    id_lieu int references Lieu(id),
+    id_lieu int references lieu(id),
     video varchar(50)
 );
 
-create table Lieu_HTML(
+create table lieu_HTML(
 	id int auto_increment  primary key,
-    id_lieu varchar(100)  references Lieu(id),
+    id_lieu int  references lieu(id),
     html varchar(50)
 );
--- database/script/20230725.sql
+++ database/script/20230725.sql
@@ -1,20 +1,20 @@
---MDP
---sedra2022
+-- MDP
+-- sedra2022
 INSERT INTO utilisateur
 (id, nom, email, motdepasse, id_langue, contact)
 VALUES(2, 'sedra', 'sedra23@gmail.com', '$2a$10$skVjtMZRhmxxzKXeSwHYMOOZXvF2oNaFxrmuRaigdlvF6eTfmqApG', 1, '0347808955');
 
---sedra2022
+-- sedra2022
 INSERT INTO utilisateur
 (id, nom, email, motdepasse, id_langue, contact)
 VALUES(5, 'sarah', 'sandatia104555@gmail.com', '$2a$10$g7MVn4GpP6ImBsHBa13bL.zhpsyVnHssY5rXbHf999GDpRMn23s7y', 1, '0347808955');
 
---1234
+-- 1234
 INSERT INTO utilisateur
 (id, nom, email, motdepasse, id_langue, contact)
 VALUES(6, 'Tsiory', 'tsiory@gmail.com', '$2a$10$JpG1s4oj9tcD8ADnEY/ioucPGMPDQHSR1IeA346FEcRArkLcJGmP.', 1, '0343456788');
 
---charle1234
+-- charle1234
 INSERT INTO utilisateur
 (id, nom, email, motdepasse, id_langue, contact)
 VALUES(7, 'charle', 'charle@gmail.com', '$2a$10$yf6powlqhG7CRX/GXneUBeMKLv5qWYgjF2YwjpcM1kAd4zEHcOpga', 1, '0343456788');
